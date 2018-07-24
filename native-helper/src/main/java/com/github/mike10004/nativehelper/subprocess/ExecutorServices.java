92da047c8fae62a9f6173d21d03d8f62377bd910
Mike Chaberski <mchaberski@gmail.com>
2018-07-24 14:27:40 -0400

    native: use guava ThreadFactoryBuilder instead of custom ThreadFactory implementation

--- native-helper/src/main/java/com/github/mike10004/nativehelper/subprocess/ExecutorServices.java
+++ native-helper/src/main/java/com/github/mike10004/nativehelper/subprocess/ExecutorServices.java
@@ -4,10 +4,11 @@ import com.google.common.base.CharMatcher;
 import com.google.common.base.Strings;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -16,33 +17,21 @@ class ExecutorServices {
 
     private ExecutorServices() {}
 
-    private static final AtomicInteger counter = new AtomicInteger(0);
+    private static final CharMatcher VALID_POOL_NAME_CHARS = CharMatcher.inRange('a', 'z')
+            .or(CharMatcher.inRange('A', 'Z'))
+            .or(CharMatcher.inRange('0', '9'))
+            .or(CharMatcher.anyOf("_-.,"));
 
     public static Supplier<ListeningExecutorService> newSingleThreadExecutorServiceFactory(String poolName) {
-        return () -> MoreExecutors.listeningDecorator(Executors.newSingleThreadExecutor(new NamedThreadFactory(poolName)));
+        checkArgument(VALID_POOL_NAME_CHARS.matchesAllOf(poolName), "pool name characters are restricted to %s", VALID_POOL_NAME_CHARS);
+        return () -> {
+            String prefix = Strings.isNullOrEmpty(poolName) ? "nh-subprocess" : poolName;
+            ThreadFactory threadFactory = new ThreadFactoryBuilder()
+                    .setNameFormat(prefix + "-%d")
+                    .build();
+            ExecutorService service = Executors.newSingleThreadExecutor(threadFactory);
+            return MoreExecutors.listeningDecorator(service);
+        };
     }
 
-    private static class NamedThreadFactory implements ThreadFactory {
-
-        private final ThreadFactory defaultFactory = Executors.defaultThreadFactory();
-        private final String namePrefix;
-
-        private NamedThreadFactory(String namePrefix) {
-            if (Strings.isNullOrEmpty(namePrefix)) {
-                namePrefix = "SubprocessExecutors";
-            }
-            checkArgument(CharMatcher.whitespace().negate().countIn(namePrefix) > 0, "name must include non-whitespace");
-            this.namePrefix = namePrefix;
-        }
-
-        @SuppressWarnings("NullableProblems")
-        @Override
-        public Thread newThread(Runnable r) {
-            Thread t = defaultFactory.newThread(r);
-            String name = String.format("%s-%d", namePrefix, counter.incrementAndGet());
-            t.setName(name);
-            t.setDaemon(false);
-            return t;
-        }
-    }
 }
